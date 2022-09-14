36e29105b46e4ad81c3ff7cec9b60bc1bcb597d5
NastyPigz <williamzwy06701@gmail.com>
2022-09-13 20:51:05 -0400

    Added captcha playground

--- src/pages/captcha.tsx
+++ src/pages/captcha.tsx
@@ -2,28 +2,19 @@ import HCaptcha from '@hcaptcha/react-hcaptcha';
 import { useRef, useState } from "react";
 
 export default function Login() {
-    const [verified, setVerified] = useState<boolean>(false);
+    const [counter, setCounter] = useState<number>(0);
     const captchaRef = useRef<HCaptcha>(null);
     return (
         <>
-            <form id="frm" onSubmit={(e) => {
-                if (!verified) {
-                    alert("Do the captcha bruh");
-                    return;
-                }
-                e.preventDefault();
-                alert("Congrats you're not a robot");
-            }}>
-                <HCaptcha
-                    ref={captchaRef}
-                    sitekey="b06e23a9-c61e-485e-a472-4b7d2e8077d2"
-                    onVerify={_ => {
-                        setVerified(true);
-                        captchaRef?.current?.execute();
-                    }}
-                />
-                <button type="submit">Submit</button>
-            </form>
+            <p>SOLVED: {counter}</p>
+            <HCaptcha
+                ref={captchaRef}
+                sitekey="b06e23a9-c61e-485e-a472-4b7d2e8077d2"
+                onVerify={_ => {
+                    setCounter(c => c+1);
+                    captchaRef?.current?.execute();
+                }}
+            />
         </>
     )
 }
